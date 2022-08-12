b908465d8406d912a01af456ea02366f826b30ce
levilu800b <levilu800b@gmail.com>
2022-08-12 14:45:32 +0100

    Correction

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 function ageInDays() {
 	var birthYear = prompt('What year were you born... Good friend?');
-	var ageInDayss = (2021 - birthYear) * 365;
+	var ageInDays = (2021 - birthYear) * 365;
 	var h1 = document.createElement('h1');
 	var textAnswer = document.createTextNode(
-		'You are ' + ageInDayss + ' days old.'
+		'You are ' + ageInDays + ' days old.',
 	);
 	h1.setAttribute('id', 'ageInDays');
 	h1.appendChild(textAnswer);
